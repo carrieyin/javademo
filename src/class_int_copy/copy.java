8f00b418ac0e7498247c3c94e7849f3db9101c5d
尹丹丹 <yindd@travelsky.com>
2019-01-12 22:52:32 +0800

    add

--- src/class_int_copy/copy.java
+++ src/class_int_copy/copy.java
@@ -1,10 +1,21 @@
 package class_int_copy;
 
+import java.util.Scanner;
+
 public class copy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		long sum = 0;
+		long number = 0;
+		do {  
+			System.out.println("input number:");
+			number = Long.parseLong(scanner.nextLine());
+			sum += number;
+		}while(number != 0);
+		System.out.println("sum:" + sum);
+		scanner.close();
 	}
 
 }
